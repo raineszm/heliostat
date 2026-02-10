bb0578d1ada3c7d1fcc24c5c6eaac14af265cf66
Zachary Raines <zachary.raines@canonical.com>
2026-02-10 17:14:48 -0600

    Add utilities for importing rocks to k8s and juju

---
+++ src/heliostat/resources/ctr.py
@@ -0,0 +1,55 @@
+"""
+Tools for adding built rocks to the local containerd registry in a way
+that they can be accessed by juju.
+"""
+
+import json
+import subprocess
+import tarfile
+
+# https://documentation.ubuntu.com/canonical-kubernetes/latest/snap/howto/image-management/
+from pathlib import Path
+
+CTR_BIN = "/snap/k8s/current/bin/ctr"
+CTR_SOCK = "/run/containerd/containerd.sock"
+K8S_NS = "k8s.io"
+
+FAKE_REGISTRY = "phantom-registry.zmraines.com"
+
+
+def ctr_cmd(*args: str) -> list[str]:
+    return [CTR_BIN, "--address", CTR_SOCK, "--namespace", K8S_NS] + list(args)
+
+
+def image_name(rock_name: str) -> str:
+    return f"{FAKE_REGISTRY}/{rock_name}"
+
+
+def image_digest(rock_path: Path) -> str:
+    with tarfile.open(rock_path, "r") as tar:
+        f = tar.extractfile("index.json")
+        if not f:
+            raise ValueError(f"Failed to extract index.json from {rock_path}")
+        parsed = json.load(f)
+        return parsed["manifests"][0]["digest"]
+
+
+def import_image(rock_path: Path, rock_name: str):
+    subprocess.check_call(
+        ctr_cmd(
+            "images",
+            "import",
+            "--base-name",
+            image_name(rock_name),
+            str(rock_path),
+        )
+    )
+
+
+def has_image(digest: str) -> bool:
+    return any(
+        line.endswith(digest)
+        for line in subprocess.check_output(ctr_cmd("images", "ls", "-q"))
+        .decode("utf-8")
+        .splitlines()
+    )
---
+++ src/heliostat/resources/juju.py
@@ -0,0 +1,30 @@
+"""Utilities for updating the oci-image resources associated with Juju k8s
+charms."""
+
+import shutil
+import subprocess
+from pathlib import Path
+
+from .ctr import has_image, image_digest, image_name, import_image
+
+JUJU_BIN = shutil.which("juju") or "/snap/juju/current/bin/juju"
+SUNBEAM_MODEL = "openstack"
+
+
+def juju_cmd(cmd: str, *args: str) -> list[str]:
+    return [JUJU_BIN, cmd, "--model", SUNBEAM_MODEL] + list(args)
+
+
+def attach_resource(charm_name: str, image_name: str, digest: str):
+    subprocess.check_call(
+        juju_cmd("attach-resource", charm_name, f"{image_name}@{digest}")
+    )
+
+
+def attach_rock(charm_name: str, rock_path: Path):
+    rock_name = rock_path.name.split("_")[0]
+    name = image_name(rock_name)
+    digest = image_digest(rock_path)
+    if not has_image(digest):
+        import_image(rock_path, rock_name)
+    attach_resource(charm_name, name, digest)

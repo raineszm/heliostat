c7a9688ce03bd2ee851b7b16f57b330fa2d43e28
Zachary Raines <zachary.raines@canonical.com>
2026-04-22 15:13:03 -0500

    test: add spread tests for `heliostat charm attach`
    Signed-off-by: Zachary Raines <zachary.raines@canonical.com>

---
+++ spread/tasks/charm-attach/minimal-charm/src/charm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Minimal test charm."""
+
+import ops
+
+
+class MinimalCharm(ops.CharmBase):
+    """Charm the application."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        framework.observe(
+            self.on["test-container"].pebble_ready, self._on_pebble_ready
+        )
+        self.container = self.unit.get_container("test-container")
+
+    def _on_pebble_ready(self, _: ops.PebbleReadyEvent):
+        """Handle pebble-ready event."""
+        self.unit.status = ops.ActiveStatus()
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(MinimalCharm)
